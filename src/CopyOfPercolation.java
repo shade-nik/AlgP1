90b4b00958a6bf0db9f8836fc2f86d9f4fb2b123
nboiko <nboiko@cybervisiontech.com>
2014-03-03 18:13:50 +0200

    ProgAssig 3... little bit

--- src/CopyOfPercolation.java
+++ src/CopyOfPercolation.java
@@ -1,6 +1,7 @@
-public class Percolation
+public class CopyOfPercolation
 {
    private boolean openMatrix[];
+   private boolean perc;
    
    private int n;
    private int top;
@@ -14,18 +15,23 @@ public class Percolation
 
    private AbstractUnionFind uf1D;
 
-   public Percolation(int n)
+   public CopyOfPercolation(int n)
    {
+      // NxN - matrix dimension
       this.n = n;
       top = n * n;
       bottom = top + 1;
 
+      // open node count
       countOpen = 0;
+      // optional neighbors array for 2d matrix
       neighbors2D = new int[4];
+      // nodes state(open/closed) storage =)
       openMatrix = new boolean[top + 2];
 
       openMatrix[top] = true;
       openMatrix[bottom] = true;
+      perc = false;
       
       uf1D = new QuickUF(top + 2);
    }
@@ -34,7 +40,10 @@ public class Percolation
    {
       while (!percolates())
       {
+
          openRandomNode();
+//         testOutOpenMatrix();
+
       }
       pTreshold = (double) countOpen / (n * n);
       return pTreshold;
@@ -48,12 +57,17 @@ public class Percolation
       {
          open(i, j);
          countOpen++;
+         if(isFull(i, j)) {
+            perc = true;
+         }
       }
 
    }
 
+   /* Check if site with [i,j] is connected to open site at the top */
    private boolean isFull(int i, int j)
    {
+      // Actually need union find there
       if (uf1D.connected(convert2DtoLinear(i, j), top))
       {
          return true;
@@ -64,35 +78,52 @@ public class Percolation
       }
    }
 
+   /**
+    * Check if site with [i,j] is opened Реализация - 0 это закрытая нода, если туда наступили - значение не 0
+    */
    private boolean isOpen(int i, int j)
    {
       return openMatrix[convert2DtoLinear(i, j)];
    }
 
+   /**
+    * open node|site... whatever, if not already open Отрыть - соединить с 2D соседями
+    */
    private void open(int i, int j)
    {
+      // Вычисляем id для linear UF
       int linearUnionId = convert2DtoLinear(i, j);
       openMatrix[linearUnionId] = true;
+      // open2Dneighbors(linearUnionId);
       calculateNeighbors(linearUnionId, i, j);
       unionWithNeighbors(linearUnionId, i, j);
    }
 
    private boolean percolates()
    {
+      // Определим что такое есть протекать, получается что это когда
+      // виртуальный топ и виртуальный bottom в одном компоненте
       for (int i = 1; i < n; i++)
       {
-         if (openMatrix[top-i] && isFull(n-1, i))
+         if (openMatrix[top-i] && uf1D.connected(top - i, top))
          {
             return true;
          }
       }
+      // if (uf1D.connected(top, bottom))
+      // {
+      // return true;
+      // }
       return false;
    }
 
    public static void main(String... args)
    {
-      Percolation perco = new Percolation(Integer.parseInt(args[0]));
+      // создаем подтекатель
+      CopyOfPercolation perco = new CopyOfPercolation(Integer.parseInt(args[0]));
+      // вычисляем коэффициент протекания =)
       double percolationTreshold = perco.computePercolation();
+      // Выводим
       System.out.println("Computed treshold: " + percolationTreshold);
    }
 
@@ -103,26 +134,37 @@ public class Percolation
 
    private void unionWithNeighbors(int linear, int i, int j)
    {
+      // Объединения с соседями хм будем 4 раза проходить весь массив
+      // left
       if (openMatrix[neighbors2D[0]])
       {
          uf1D.union(linear, neighbors2D[0]);
       }
+      // right
       if (openMatrix[neighbors2D[1]])
       {
          uf1D.union(linear, neighbors2D[1]);
       }
+      // top
       if (openMatrix[neighbors2D[2]])
       {
          uf1D.union(linear, neighbors2D[2]);
       }
+      // bottom
       if (openMatrix[neighbors2D[3]])
       {
+
          uf1D.union(linear, neighbors2D[3]);
       }
    }
 
    private void calculateNeighbors(int linear, int i, int j)
    {
+      /**
+       * соединяем ноды так если мы получаем меньше нуля сверху - соединяем с виртуальным topId если больше n^2 (n*n)
+       * снизу - соединяем с виртуальным bottomId
+       */
+
       if (j == 0)
       {
          neighbors2D[0] = linear;
@@ -131,6 +173,9 @@ public class Percolation
       {
          neighbors2D[0] = linear - 1;
       }
+      /*
+       * right neighbor if (j==n) doesn't exist (no node to the right) than set to 0
+       */
       if (j == n - 1)
       {
          neighbors2D[1] = linear;
@@ -139,6 +184,9 @@ public class Percolation
       {
          neighbors2D[1] = linear + 1;
       }
+      /*
+       * i == 0) set to 0
+       */
       if (i == 0)
       {
          neighbors2D[2] = top; // ?
@@ -157,4 +205,19 @@ public class Percolation
          neighbors2D[3] = linear + n;
       }
    }
+
+   private void testOutOpenMatrix()
+   {
+      System.out.println("Sites opened:" + countOpen);
+      for (int i = 1; i < n * n + 1; i++)
+      {
+         System.out.print("[" + ((openMatrix[i - 1]) ? "*" : " ") + "]");
+
+         if (i % n == 0)
+         {
+            System.out.println();
+         }
+      }
+   }
+
 }
---
+++ src/Fast.java
@@ -0,0 +1,22 @@
+public class Fast
+{
+   private int n;
+   private Point[] points;
+   
+   public void findFast() {
+   
+      
+   }
+   
+   public Fast(In in)
+   {
+   }
+   
+   
+   
+   public static void main(String[] args)
+   {
+      In in = new In();
+      Fast f = new Fast(in);
+   }
+}
